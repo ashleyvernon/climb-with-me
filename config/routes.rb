743d9ee9f15d32d735ae423f419dde678d911571
ashleyvernon <avernon526@gmail.com>
2016-10-29 18:53:03 -0700

    adds mailboxer routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
 	root to: "home#index"
 
+	get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+	get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+	get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+
 end
