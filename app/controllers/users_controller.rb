ead7bbd3072c1921403dfa148679d7068e8c6ab7
ashleyvernon <avernon526@gmail.com>
2016-11-02 09:52:05 -0700

    adds user id to user parmas private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,44 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all
+		
 	end
 
 	def new
 		@user = User.new
+
 	end
 
 	def create 
 		@user = User.create(user_params)
-
-		if user.save
-			redirect_to users_path
+		login(@user)
+		if @user.save
+			redirect_to @user
 		end
+		
 	end
 
 	def show 
 		user_id = params[:id]
-		@user = User.find_by_id(user_path)
+		@user = User.find_by_id(user_id)
 	end
 
 	def edit
+		user_id = params[:id]
+		@user = User.find_by_id(user_id)
 	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to user_registration_path
+		end
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:user_id, :name, :email, :password, :gym)
+	end
+
 end
