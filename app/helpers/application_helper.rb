8e34518a5b24bc1b209f632eb7d3b900f4719086
ashleyvernon <avernon526@gmail.com>
2016-11-02 10:28:12 -0700

    adds resource method in helper for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,18 @@ module ApplicationHelper
 			when :warning then "alert alert-warning"
 		end
 	end
+	
+	def resource_name
+		:user
+	end
+
+	def resource
+		@user ||= User.new(params[:id])
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
 
 	def active_page(active_page)
 		@active == active_page ? "active" : ""
